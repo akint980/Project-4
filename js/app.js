a3c139905f67ece8ca80ff6cb3ec760b71d4fb13
akint980 <akintunde_aderonke@yahoo.com>
2024-10-10 15:58:11 -0500

    updating css and js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-$(document).foundation()
+ $(document).foundation();
+
+    // Example JavaScript functionality
+    $(document).ready(function() {
+      // Highlight sections on hover
+      $('.section').hover(
+        function() {
+          $(this).css('background-color', '#e9ecef');
+        },
+        function() {
+          $(this).css('background-color', 'white');
+        }
+      );
+    });
